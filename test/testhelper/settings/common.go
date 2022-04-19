c2c19b8b1d1ea0b108e7a2e436fa91696102fa44
Stevent-fei <65949637+stevent-fei@users.noreply.github.com>
2022-04-19 11:25:59 +0800

    Update test

---
+++ test/testhelper/settings/common.go
@@ -0,0 +1,60 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package settings
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+const (
+	DefaultRegistryAuthFileDir = "/root/.docker"
+)
+
+const (
+	BAREMETAL         = "BAREMETAL"
+	AliCloud          = "ALI_CLOUD"
+	ClusterNameForRun = "my-cluster"
+	ClusterWorkDir    = "/root/.sealer/%s"
+)
+
+var (
+	DefaultPollingInterval time.Duration
+	MaxWaiteTime           time.Duration
+	DefaultWaiteTime       time.Duration
+	DefaultEnv             = "PodCIDR=172.45.0.0/16,SvcCIDR=10.96.0.0/16,Network=calico,EtcdDevice=/dev/vdb,DockerRunDiskSize=200,KubeletRunDiskSize=200,StorageDevice=/dev/vdc,YodaDevice=/dev/vdc3"
+	DefaultSealerBin       = ""
+	DefaultTestEnvDir      = ""
+	RegistryURL            = os.Getenv("REGISTRY_URL")
+	RegistryUsername       = os.Getenv("REGISTRY_USERNAME")
+	RegistryPasswd         = os.Getenv("REGISTRY_PASSWORD")
+	TestImageName          = "ack-distro:test" //default: registry.cn-qingdao.aliyuncs.com/sealer-io/kubernetes:v1.19.8
+)
+
+func GetClusterWorkDir(clusterName string) string {
+	home, err := homedir.Dir()
+	if err != nil {
+		return fmt.Sprintf(ClusterWorkDir, clusterName)
+	}
+	return filepath.Join(home, ".sealer", clusterName)
+}
+
+func GetClusterWorkClusterfile(clusterName string) string {
+	return filepath.Join(GetClusterWorkDir(clusterName), "Clusterfile")
+}
---
+++ test/testhelper/utils.go
@@ -0,0 +1,106 @@
+// Copyright © 2021 Alibaba Group Holding Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testhelper
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/onsi/gomega"
+	"sigs.k8s.io/yaml"
+
+	"github.com/alibaba/sealer/test/testhelper/settings"
+	v1 "github.com/alibaba/sealer/types/api/v1"
+	"github.com/alibaba/sealer/utils"
+	"github.com/alibaba/sealer/utils/ssh"
+)
+
+func CreateTempFile() string {
+	dir := os.TempDir()
+	file, err := ioutil.TempFile(dir, "tmpfile")
+	CheckErr(err)
+	defer CheckErr(file.Close())
+	return file.Name()
+}
+
+func RemoveTempFile(file string) {
+	CheckErr(os.Remove(file))
+}
+
+func WriteFile(fileName string, content []byte) error {
+	dir := filepath.Dir(fileName)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err = os.MkdirAll(dir, settings.FileMode0755); err != nil {
+			return err
+		}
+	}
+
+	if err := ioutil.WriteFile(fileName, content, settings.FileMode0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+type SSHClient struct {
+	RemoteHostIP string
+	SSH          ssh.Interface
+}
+
+func NewSSHClientByCluster(usedCluster *v1.Cluster) *SSHClient {
+	sshClient, err := ssh.NewSSHClientWithCluster(usedCluster)
+	CheckErr(err)
+	CheckNotNil(sshClient)
+	return &SSHClient{
+		RemoteHostIP: sshClient.Host,
+		SSH:          sshClient.SSH,
+	}
+}
+
+func MarshalYamlToFile(file string, obj interface{}) error {
+	data, err := yaml.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	if err = WriteFile(file, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteFileLocally delete file for cloud apply
+func DeleteFileLocally(filePath string) {
+	cmd := fmt.Sprintf("sudo -E rm -rf %s", filePath)
+	_, err := utils.RunSimpleCmd(cmd)
+	CheckErr(err)
+}
+
+func CheckErr(err error) {
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+}
+
+func CheckNotNil(obj interface{}) {
+	gomega.Expect(obj).NotTo(gomega.BeNil())
+}
+
+func CheckEqual(obj1 interface{}, obj2 interface{}) {
+	gomega.Expect(obj1).To(gomega.Equal(obj2))
+}
+
+func CheckFuncBeTrue(f func() bool, t time.Duration) {
+	gomega.Eventually(f(), t).Should(gomega.BeTrue())
+}
