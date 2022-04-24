d1d8738e0c79b83132124c418019e28dbdc5c465
wangfei <wf01031309@alibaba-inc.com>
2022-04-24 18:26:06 +0800

    apply 4

--- test/testhelper/utils.go
+++ test/testhelper/utils.go
@@ -123,7 +123,7 @@ func CheckErr(err error) {
 }
 
 func CheckNotNil(obj interface{}) {
-	gomega.Expect(obj).NotTo(gomega.BeNil())
+	gomega.Expect(obj).To(gomega.BeNil())
 }
 
 func CheckEqual(obj1 interface{}, obj2 interface{}) {
