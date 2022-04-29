30d8500ff4b510b648474b7c33563fce8b9a48d1
wangfei <wf01031309@alibaba-inc.com>
2022-04-29 14:10:37 +0800

    run

--- test/sealer_apply_test.go
+++ test/sealer_apply_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"blog/test/testhelper"
 	. "github.com/onsi/ginkgo"
+	"strings"
 
 	"blog/test/suites/apply"
 	"blog/test/testhelper/settings"
@@ -46,7 +47,6 @@ var _ = Describe("sealer apply", func() {
 				By("start to init cluster")
 				apply.GenerateClusterfile(tempFile)
 				apply.SendAndApplyCluster(sshClient, tempFile)
-				apply.CheckNodeNumWithSSH(sshClient, 2)
 
 				By("Wait for the cluster to be ready", func() {
 					apply.WaitAllNodeRunningBySSH(sshClient.SSH,sshClient.RemoteHostIP)
@@ -54,56 +54,62 @@ var _ = Describe("sealer apply", func() {
 				By("start to delete cluster")
 				err := sshClient.SSH.CmdAsync(sshClient.RemoteHostIP, apply.SealerDeleteCmd(tempFile))
 				testhelper.CheckErr(err)
-			})
-		})
-	})
-
-	Context("start apply hybridnet", func() {
-		rawClusterFilePath := apply.GetRawClusterFilePath()
-		rawCluster := apply.LoadClusterFileFromDisk(rawClusterFilePath)
-		rawCluster.Spec.Image = settings.TestImageName
-		rawCluster.Spec.Env = settings.HybridnetEnv
-		BeforeEach(func() {
-			if rawCluster.Spec.Image != settings.TestImageName {
-				rawCluster.Spec.Image = settings.TestImageName
-				apply.MarshalClusterToFile(rawClusterFilePath, rawCluster)
-			}
-		})
-
-		Context("check regular scenario that provider is bare metal, executes machine is master0", func() {
-			var tempFile string
-			BeforeEach(func() {
-				tempFile = testhelper.CreateTempFile()
-			})
-
-			AfterEach(func() {
-				testhelper.RemoveTempFile(tempFile)
-			})
-			It("init, clean up", func() {
-				By("start to prepare infra")
-				cluster := rawCluster.DeepCopy()
-				cluster.Spec.Provider = settings.AliCloud
-				cluster.Spec.Image = settings.TestImageName
-				cluster = apply.CreateAliCloudInfraAndSave(cluster, tempFile)
-				defer apply.CleanUpAliCloudInfra(cluster)
-				sshClient := testhelper.NewSSHClientByCluster(cluster)
-				testhelper.CheckFuncBeTrue(func() bool {
-					err := sshClient.SSH.Copy(sshClient.RemoteHostIP, settings.DefaultSealerBin, settings.DefaultSealerBin)
-					return err == nil
-				}, settings.MaxWaiteTime)
-
-				By("start to init cluster")
-				apply.GenerateClusterfile(tempFile)
-				apply.SendAndApplyCluster(sshClient, tempFile)
-				apply.CheckNodeNumWithSSH(sshClient, 2)
-
-				By("Wait for the cluster to be ready", func() {
-					apply.WaitAllNodeRunningBySSH(sshClient.SSH,sshClient.RemoteHostIP)
+				By("sealer run calico", func() {
+					masters := strings.Join(cluster.Spec.Masters.IPList, ",")
+					nodes := strings.Join(cluster.Spec.Nodes.IPList, ",")
+					apply.SendAndRunCluster(sshClient, tempFile, masters, nodes, cluster.Spec.SSH.Passwd)
+					apply.CheckNodeNumWithSSH(sshClient, 2)
 				})
-				By("start to delete cluster")
-				err := sshClient.SSH.CmdAsync(sshClient.RemoteHostIP, apply.SealerDeleteCmd(tempFile))
-				testhelper.CheckErr(err)
 			})
 		})
 	})
+
+	//Context("start apply hybridnet", func() {
+	//	rawClusterFilePath := apply.GetRawClusterFilePath()
+	//	rawCluster := apply.LoadClusterFileFromDisk(rawClusterFilePath)
+	//	rawCluster.Spec.Image = settings.TestImageName
+	//	rawCluster.Spec.Env = settings.HybridnetEnv
+	//	BeforeEach(func() {
+	//		if rawCluster.Spec.Image != settings.TestImageName {
+	//			rawCluster.Spec.Image = settings.TestImageName
+	//			apply.MarshalClusterToFile(rawClusterFilePath, rawCluster)
+	//		}
+	//	})
+	//
+	//	Context("check regular scenario that provider is bare metal, executes machine is master0", func() {
+	//		var tempFile string
+	//		BeforeEach(func() {
+	//			tempFile = testhelper.CreateTempFile()
+	//		})
+	//
+	//		AfterEach(func() {
+	//			testhelper.RemoveTempFile(tempFile)
+	//		})
+	//		It("init, clean up", func() {
+	//			By("start to prepare infra")
+	//			cluster := rawCluster.DeepCopy()
+	//			cluster.Spec.Provider = settings.AliCloud
+	//			cluster.Spec.Image = settings.TestImageName
+	//			cluster = apply.CreateAliCloudInfraAndSave(cluster, tempFile)
+	//			defer apply.CleanUpAliCloudInfra(cluster)
+	//			sshClient := testhelper.NewSSHClientByCluster(cluster)
+	//			testhelper.CheckFuncBeTrue(func() bool {
+	//				err := sshClient.SSH.Copy(sshClient.RemoteHostIP, settings.DefaultSealerBin, settings.DefaultSealerBin)
+	//				return err == nil
+	//			}, settings.MaxWaiteTime)
+	//
+	//			By("start to init cluster")
+	//			apply.GenerateClusterfile(tempFile)
+	//			apply.SendAndApplyCluster(sshClient, tempFile)
+	//			apply.CheckNodeNumWithSSH(sshClient, 2)
+	//
+	//			By("Wait for the cluster to be ready", func() {
+	//				apply.WaitAllNodeRunningBySSH(sshClient.SSH,sshClient.RemoteHostIP)
+	//			})
+	//			By("start to delete cluster")
+	//			err := sshClient.SSH.CmdAsync(sshClient.RemoteHostIP, apply.SealerDeleteCmd(tempFile))
+	//			testhelper.CheckErr(err)
+	//		})
+	//	})
+	//})
 })
