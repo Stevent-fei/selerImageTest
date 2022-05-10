f1dcda57e3f09050a6ea6f7554b4cbf8b5c127e3
wangfei <wf01031309@alibaba-inc.com>
2022-05-10 10:30:35 +0800

    run

--- test/sealer_calico_test.go
+++ test/sealer_calico_test.go
@@ -11,7 +11,7 @@ import (
 	"blog/test/testhelper/settings"
 )
 
-var _ = Describe("run calico", func() {
+var _ = Describe("run calico ", func() {
 	Context("start apply calico", func() {
 		rawClusterFilePath := apply.GetRawClusterFilePath()
 		rawCluster := apply.LoadClusterFileFromDisk(rawClusterFilePath)
@@ -56,12 +56,33 @@ var _ = Describe("run calico", func() {
 
 				By("apply.SealerDelete()")
 				time.Sleep(20 *time.Second)
+
 				By("sealer run calico")
 				masters := strings.Join(cluster.Spec.Masters.IPList, ",")
 				nodes := strings.Join(cluster.Spec.Nodes.IPList, ",")
 				apply.SendAndRunCluster(sshClient, tempFile, masters, nodes, cluster.Spec.SSH.Passwd)
 				apply.CheckNodeNumWithSSH(sshClient, 2)
 				fmt.Println("test finish")
+				//
+				//By("exec e2e test")
+				////下载e2e镜像包
+				//apply.GetE2eTest()
+				////将kubernetes_e2e_images_v1.20.0.tar传输到孤岛环境，在每个k8s节点上执行docker load
+				//err = sshClient.SSH.Copy(cluster.Spec.Masters.IPList[0], settings.LoadPath, settings.LoadPath)
+				//if err != nil {
+				//	return
+				//}
+				//err = sshClient.SSH.Copy(cluster.Spec.Nodes.IPList[0], settings.LoadPath, settings.LoadPath)
+				//if err != nil {
+				//	return
+				//}
+				//
+				////进入到master节点并且执行load.sh
+				//
+				//_ = fmt.Sprintf("ssh root@%s", cluster.Spec.Masters.IPList[0])
+				//_ = fmt.Sprintf("sh load.sh")
+				//os.Exit(1)
+				////进入到node节点
 			})
 		})
 	})
