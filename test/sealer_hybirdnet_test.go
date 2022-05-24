2bfd982b2460eb06e69ba06e005fdeebe611a4e0
wangfei <wf01031309@alibaba-inc.com>
2022-05-24 10:17:07 +0800

    calico

--- test/sealer_hybirdnet_test.go
+++ test/sealer_hybirdnet_test.go
@@ -93,7 +93,7 @@ var _ = Describe("run hybirdnet", func() {
 
 				//给定执行权限 && 下载并执行脚本
 				err = sshClient.SSH.CmdAsync(sshClient.RemoteHostIP, "sudo cp .kube/config /tmp/kubeconfig", "chmod 777 /tmp/kubeconfig",
-					"wget https://sealer.oss-cn-beijing.aliyuncs.com/e2e/run.sh", "wget https://sealer.oss-cn-beijing.aliyuncs.com/e2e/get-log.sh", "bash run.sh", "bash get-log.sh")
+					"wget https://sealer.oss-cn-beijing.aliyuncs.com/e2e/run.sh", "wget https://sealer.oss-cn-beijing.aliyuncs.com/e2e/get-log.sh", "bash run.sh & bash get-log.sh")
 				testhelper.CheckErr(err)
 			})
 		})
