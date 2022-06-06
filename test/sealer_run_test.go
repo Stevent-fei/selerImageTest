96d7ee4b5e8b5f0ca74c178f649460711915f512
wangfei <wf01031309@alibaba-inc.com>
2022-06-06 22:28:52 +0800

    calico

--- test/sealer_run_test.go
+++ test/sealer_run_test.go
@@ -1,62 +1,41 @@
 package test
 
 import (
-	"blog/test/suites/apply"
-	"blog/test/testhelper"
-	"blog/test/testhelper/settings"
 	. "github.com/onsi/ginkgo"
-	"strings"
 )
 
-var _ = Describe("sealer run", func() {
-	Context("start apply hybirdnet", func() {
-		rawClusterFilePath := apply.GetRawClusterFilePath()
-		rawCluster := apply.LoadClusterFileFromDisk(rawClusterFilePath)
-		rawCluster.Spec.Image = settings.TestImageName
-		rawCluster.Spec.Env = settings.HybridnetEnv
-		BeforeEach(func() {
-			if rawCluster.Spec.Image != settings.TestImageName {
-				rawCluster.Spec.Image = settings.TestImageName
-				apply.MarshalClusterToFile(rawClusterFilePath, rawCluster)
-			}
-		})
+var _ = Describe("test haha", func() {
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
+	By("hehehhehehehehehhe")
 
-		Context("executes machine is master0", func() {
-			var tempFile string
-			BeforeEach(func() {
-				tempFile = testhelper.CreateTempFile()
-			})
-
-			AfterEach(func() {
-				testhelper.RemoveTempFile(tempFile)
-			})
-			It("init, clean up", func() {
-				By("start to prepare infra")
-				cluster := rawCluster.DeepCopy()
-				cluster.Spec.Provider = settings.AliCloud
-				cluster.Spec.Image = settings.TestImageName
-				cluster = apply.CreateAliCloudInfraAndSave(cluster, tempFile)
-				defer apply.CleanUpAliCloudInfra(cluster)
-				sshClient := testhelper.NewSSHClientByCluster(cluster)
-				testhelper.CheckFuncBeTrue(func() bool {
-					err := sshClient.SSH.Copy(sshClient.RemoteHostIP, settings.DefaultSealerBin, settings.DefaultSealerBin)
-					return err == nil
-				}, settings.MaxWaiteTime)
-
-				By("start to init cluster")
-				apply.GenerateClusterfile(tempFile)
-				apply.SendAndApplyCluster(sshClient, tempFile)
-
-				By("start to delete cluster")
-				err := sshClient.SSH.CmdAsync(sshClient.RemoteHostIP, apply.SealerDeleteCmd(tempFile))
-				testhelper.CheckErr(err)
-
-				By("sealer run hybirdnet")
-				masters := strings.Join(cluster.Spec.Masters.IPList, ",")
-				nodes := strings.Join(cluster.Spec.Nodes.IPList, ",")
-				apply.SendAndRunHybirdnetCluster(sshClient, tempFile, masters, nodes, cluster.Spec.SSH.Passwd)
-				apply.CheckNodeNumWithSSH(sshClient, 2)
-			})
-		})
-	})
 })
+
+var _ = Describe("test xixi", func() {
+	Context("start apply hybirdnet", func() {
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+		By("this is xixixi")
+	})
+}
