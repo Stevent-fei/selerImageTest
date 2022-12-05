0bec41d260c25f3dbc4ecaa88b7445c09e7e4fc1
wangfei <wf01031309@alibaba-inc.com>
2022-12-05 15:43:14 +0800

    auto build

--- auto.sh
+++ auto.sh
@@ -102,7 +102,7 @@ pauseImage=$(./"${ARCH}"/bin/kubeadm config images list --config "rootfs/etc/kub
 if [ -f "rootfs/etc/dump-config.toml" ]; then sudo sed -i "s/sea.hub:5000\/pause:3.6/$(echo "$pauseImage" | sed 's/\//\\\//g')/g" rootfs/etc/dump-config.toml; fi
 sudo sed -i "s/v1.19.8/${k8s_version}/g" {arm64,amd64}/etc/Metadata
 ##linux/arm64,linux/amd64
-sudo ./sealer build -t "${buildName}" -f Kubefile --platform "${platform}" .
+sudo sealer build -t "${buildName}" -f Kubefile
 if [[ "$push" == "true" ]]; then
   if [[ -n "$username" ]] && [[ -n "$password" ]]; then
     sudo ./sealer login "$(echo "$buildName" | cut -d "/" -f1)" -u "${username}" -p "${password}"
