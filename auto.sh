134ef762273ebbdcf5fcbc9b3c7d6f3555973e28
wangfei <wf01031309@alibaba-inc.com>
2022-12-08 14:30:03 +0800

    auto build

--- auto.sh
+++ auto.sh
@@ -105,7 +105,7 @@ sudo sed -i "s/v1.19.8/${k8s_version}/g" {arm64,amd64}/etc/Metadata
 sudo sealer build -t "docker.io/18791106690/kubernetes:${k8s_version}" -f Kubefile
 if [[ "$push" == "true" ]]; then
   if [[ -n "$username" ]] && [[ -n "$password" ]]; then
-    sudo ./sealer login "$(echo "$buildName" | cut -d "/" -f1)" -u "${username}" -p "${password}"
+    sudo sealer login "$(echo "docker.io" | cut -d "/" -f1)" -u "${username}" -p "${password}"
   fi
-  sudo ./sealer push "docker.io/18791106690/kubernetes:${k8s_version}"
+  sudo sealer push "docker.io/18791106690/kubernetes:${k8s_version}"
 fi
