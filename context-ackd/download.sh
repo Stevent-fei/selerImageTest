c65d2d4c4c83a9828d34a5e8c2fcd6d57f11a141
wangfei <wf01031309@alibaba-inc.com>
2023-01-04 11:57:38 +0800

    添加docker的tar包，支持自动化构建cri

--- context-ackd/download.sh
+++ context-ackd/download.sh
@@ -82,7 +82,6 @@ wget -q "${seautil_tarball_amd64_url}" && tar zxvf "${seautil_tarball_amd64}" -C
 wget -q "${seautil_tarball_arm64_url}" && tar zxvf "${seautil_tarball_arm64}" -C "arm64/bin"
 
 echo "download cri with ${cri}"
-wget -q
 wget -q "${cri_tarball_amd64_url}" && mv "${cri_tarball_amd64}" "amd64/cri/docker.tar.gz"
 wget -q "${cri_tarball_arm64_url}" && mv "${cri_tarball_arm64}" "arm64/cri/docker.tar.gz"
 
